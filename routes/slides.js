6ff625be6cf90b2d603996e08bf506d5e2abd4ff
LukasPJ <ljorgensen@winterleafentertainment.com>
2016-08-03 02:35:25 +0200

    Day-8

--- routes/slides.js
+++ routes/slides.js
@@ -4,7 +4,7 @@
 var express = require('express');
 var router = express.Router();
 
-[1, 2, 3, 4, 5, 6, 7].forEach((current, index) => {
+[1, 2, 3, 4, 5, 6, 7, 8].forEach((current, index) => {
     router.get('/day-' + current, function(req, res, next) {
         res.render('slides/day-' + current, { title: 'MOB: Slides day ' + current });
     });
